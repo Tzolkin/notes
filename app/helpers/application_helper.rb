d15c102d0ee8866279f9a25c44186ac104e687ef
Francisco Zavala <fzavaladev@gmail.com>
2019-07-31 17:55:35 -0500

    Fixed offenses and refactor folder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def flash_class(level)
-    {
-      notice:  'alert alert-info',
-      success: 'alert alert-success',
-      alert:   'alert alert-warning',
-      error:   'alert alert-danger'
-    }[level.to_sym]
-  end
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -21,47 +21,13 @@ class Folder < ApplicationRecord
       end
     end
     if child.notes.present?
-      items << child.notes.map { |note| { text: note.name, icon: 'glyphicon glyphicon-file', folderid: note.folder.id } }
+      child.notes.map do |note|
+        items << { text: note.name, icon: 'glyphicon glyphicon-file', folderid: note.folder.id }
+      end
     end
     item = { text: child.name, folderid: child.id }
     item[:nodes] = items.flatten if items.present?
 
     item
-  end
-
-  # TODO: Delete me
-  def all_children_sql
-    Folder.find_by_sql(recursive_tree_children_sql)
-  end
-
-  private
-
-  def recursive_tree_children_sql
-    columns = self.class.column_names
-    columns_joined = columns.join(',')
-    sql =
-      <<-SQL
-        WITH RECURSIVE folder_tree (#{columns_joined}, level)
-        AS (
-          SELECT
-            #{columns_joined},
-            0
-          FROM folders
-          WHERE id = #{id}
-
-          UNION ALL
-
-          SELECT
-            #{columns.map { |col| 'folders.' + col }.join(',')},
-            folder_tree.level + 1
-          FROM folders, folder_tree
-          WHERE folders.folder_id = folder_tree.id
-        )
-        SELECT  *
-        FROM    folder_tree
-        WHERE   level > 0
-        ORDER BY level, folder_id, name;
-      SQL
-    sql.chomp
   end
 end
